10c492972e91ca98777814b484e17d854c605a48
benpee <74276700+benpee@users.noreply.github.com>
2020-12-20 06:09:56 +0100

    Add files via upload

---
+++ QuizApp/precache-manifest.e799cc3226d94cfbb537fdeb79fe7642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc624cd88c51f2a62690f342846b44c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82758773d5e66fcde351",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "adf72ac8e8d3d4ba5074",
+    "url": "/static/css/main.dc7123b6.chunk.css"
+  },
+  {
+    "revision": "82758773d5e66fcde351",
+    "url": "/static/js/2.d0a4343e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0a4343e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf72ac8e8d3d4ba5074",
+    "url": "/static/js/main.84d8158c.chunk.js"
+  },
+  {
+    "revision": "2ae6e346d186b5e5b788",
+    "url": "/static/js/runtime-main.ca8a46de.js"
+  }
+]);
